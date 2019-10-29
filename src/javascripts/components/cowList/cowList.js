fbfb3776ef5aed767338055c40ea05fd44437a2c
John Thielman <john.thielman@gmail.com>
2019-10-28 19:01:25 -0500

    added axios stuff

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -1,10 +1,16 @@
-import cows from '../../helpers/data/cows';
+import cowData from '../../helpers/data/cows';
 import './cowList.scss';
 
 const buildCows = () => {
-  cows.getCows()
+  cowData.getCows()
     .then((response) => {
-      console.log('it worked!', response.data);
+      const demCows = response.data.cows;
+      const cows = [];
+      Object.keys(demCows).forEach((cowId) => { // makes an array of the keys and does a forEach loop on it
+        demCows[cowId].id = cowId; // adds id into each object
+        cows.push(demCows[cowId]); // pushes each object into cows array
+      });
+      console.log('it worked!', cows);
     })
     .catch((error) => {
       console.error('shit broke.', error);
