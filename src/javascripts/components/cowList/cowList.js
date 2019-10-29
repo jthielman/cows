f35284e975238ad1ba432541adfc4d3ad4f19aa7
John Thielman <john.thielman@gmail.com>
2019-10-28 19:14:36 -0500

    made getCows a promise

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -3,14 +3,10 @@ import './cowList.scss';
 
 const buildCows = () => {
   cowData.getCows()
-    .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => { // makes an array of the keys and does a forEach loop on it
-        demCows[cowId].id = cowId; // adds id into each object
-        cows.push(demCows[cowId]); // pushes each object into cows array
-      });
-      console.log('it worked!', cows);
+    .then((cows) => {
+      console.log('cow array from cowlist', cows);
+
+      // call a domStringBuilder and pass in cows
     })
     .catch((error) => {
       console.error('shit broke.', error);
--- src/javascripts/helpers/data/cows.js
+++ src/javascripts/helpers/data/cows.js
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 // const getCows = () => axios.get('https://teamtreehouse.com/ashleyclaiborne.json');
 
-const getCows = () => axios.get('./../../../db/cows.json');
+const getCows = () => new Promise((resolve, reject) => {
+  axios.get('./../../../db/cows.json').then((response) => {
+    const demCows = response.data.cows;
+    const cows = [];
+    Object.keys(demCows).forEach((cowId) => { // makes an array of the keys and does a forEach loop on it
+      demCows[cowId].id = cowId; // adds id into each object
+      cows.push(demCows[cowId]); // pushes each object into cows array
+    });
+    resolve(cows);
+  }).catch((error) => reject(error));
+});
 
 export default { getCows };
