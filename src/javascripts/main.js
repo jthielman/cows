a9b8d649b17dde95dd9ed62cc07358fb089a4d86
John Thielman <john.thielman@gmail.com>
2019-10-28 20:58:58 -0500

    got farmer cow data

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,5 @@
 import cowList from './components/cowList/cowList';
+import farmerCowData from './helpers/data/farmerCowData';
 import farmerList from './components/farmerList/farmerList';
 import '../styles/main.scss';
 import 'bootstrap';
@@ -6,6 +7,14 @@ import 'bootstrap';
 const init = () => {
   cowList.buildCows();
   farmerList.buildFarmers();
+
+  farmerCowData.getFarmerCows()
+    .then((farmerCows) => {
+      // farmerCows should be an array of farmercow objects
+      // they should also have the farmerCow id in them
+      console.log('from main.js', farmerCows);
+    })
+    .catch((error) => console.error(error));
 };
 
 init();
